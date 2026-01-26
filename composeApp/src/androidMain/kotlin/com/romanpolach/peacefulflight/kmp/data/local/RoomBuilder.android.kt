bc835629967f8c13417637135e59ecf75e98dd9f
Roman <roman.polach@cyberfox.cz>
2026-01-26 20:33:12 +0100

    fixed Room db

--- composeApp/src/androidMain/kotlin/com/romanpolach/peacefulflight/kmp/data/local/RoomBuilder.android.kt
+++ composeApp/src/androidMain/kotlin/com/romanpolach/peacefulflight/kmp/data/local/RoomBuilder.android.kt
@@ -3,6 +3,8 @@ package com.romanpolach.peacefulflight.kmp.data.local
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.coroutines.Dispatchers
 
 actual class RoomBuilder(private val context: Context) {
     actual fun builder(): RoomDatabase.Builder<AppDatabase> {
@@ -10,6 +12,7 @@ actual class RoomBuilder(private val context: Context) {
         return Room.databaseBuilder<AppDatabase>(
             context = context.applicationContext,
             name = dbFile.absolutePath
-        )
+        ).setDriver(BundledSQLiteDriver())
+            .setQueryCoroutineContext(Dispatchers.IO)
     }
 }
--- composeApp/src/iosMain/kotlin/com/romanpolach/peacefulflight/kmp/data/local/RoomBuilder.ios.kt
+++ composeApp/src/iosMain/kotlin/com/romanpolach/peacefulflight/kmp/data/local/RoomBuilder.ios.kt
@@ -2,14 +2,17 @@ package com.romanpolach.peacefulflight.kmp.data.local
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import platform.Foundation.NSHomeDirectory
 
 actual class RoomBuilder {
     actual fun builder(): RoomDatabase.Builder<AppDatabase> {
         val dbFilePath = NSHomeDirectory() + "/peacefulflight.db"
         return Room.databaseBuilder<AppDatabase>(
-            name = dbFilePath,
-            factory = { AppDatabase::class.instantiateImpl() }
-        )
+            name = dbFilePath
+        ).setDriver(BundledSQLiteDriver())
+            .setQueryCoroutineContext(Dispatchers.IO)
     }
 }
