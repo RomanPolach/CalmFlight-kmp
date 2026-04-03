10e1eb0c9a4a0b41e26a70af4c11fa647026836d
Roman <roman.polach@cyberfox.cz>
2026-04-04 00:32:18 +0200

    Refactor iOS speech manager delegate

--- composeApp/src/iosMain/kotlin/com/romanpolach/peacefulflight/kmp/utils/IosTtsManager.kt
+++ composeApp/src/iosMain/kotlin/com/romanpolach/peacefulflight/kmp/utils/IosTtsManager.kt
@@ -2,7 +2,6 @@ package com.romanpolach.peacefulflight.kmp.utils
 
 import com.romanpolach.peacefulflight.kmp.data.preferences.SettingsRepository
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.ObjCSignatureOverride
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -14,16 +13,22 @@ import platform.AVFAudio.AVSpeechUtterance
 import platform.AVFAudio.AVSpeechUtteranceDefaultSpeechRate
 import platform.AVFAudio.AVSpeechUtteranceMaximumSpeechRate
 import platform.AVFAudio.AVSpeechUtteranceMinimumSpeechRate
+import kotlinx.cinterop.ObjCSignatureOverride
 import platform.darwin.NSObject
 
 @OptIn(ExperimentalForeignApi::class)
 class IosTtsManager(
     private val settingsRepository: SettingsRepository
-) : NSObject(), TtsManager, AVSpeechSynthesizerDelegateProtocol {
+) : TtsManager {
 
     private val synthesizer = AVSpeechSynthesizer()
     private val _availableVoices = MutableStateFlow<List<TtsVoiceOption>>(emptyList())
     private val _isSpeaking = MutableStateFlow(false)
+    private val speechDelegate = SpeechDelegate(
+        onStart = { _isSpeaking.value = true },
+        onFinish = { _isSpeaking.value = false },
+        onCancel = { _isSpeaking.value = false }
+    )
 
     override val availableVoices: StateFlow<List<TtsVoiceOption>> = _availableVoices.asStateFlow()
 
@@ -31,7 +36,7 @@ class IosTtsManager(
     private var speechRate: Float = settingsRepository.getTtsSpeechRate()
 
     init {
-        synthesizer.delegate = this
+        synthesizer.delegate = speechDelegate
         refreshAvailableVoices()
         restoreSavedVoice()
     }
@@ -84,32 +89,6 @@ class IosTtsManager(
         settingsRepository.setTtsSpeechRate(speechRate)
     }
 
-    @ObjCSignatureOverride
-    override fun speechSynthesizer(
-        synthesizer: AVSpeechSynthesizer,
-        didStartSpeechUtterance: AVSpeechUtterance
-    ) {
-        _isSpeaking.value = true
-    }
-
-    @ObjCSignatureOverride
-    override fun speechSynthesizer(
-        synthesizer: AVSpeechSynthesizer,
-        didFinishSpeechUtterance: AVSpeechUtterance
-    ) {
-        if (!synthesizer.speaking) {
-            _isSpeaking.value = false
-        }
-    }
-
-    @ObjCSignatureOverride
-    override fun speechSynthesizer(
-        synthesizer: AVSpeechSynthesizer,
-        didCancelSpeechUtterance: AVSpeechUtterance
-    ) {
-        _isSpeaking.value = false
-    }
-
     private fun refreshAvailableVoices() {
         _availableVoices.value = AVSpeechSynthesisVoice
             .speechVoices()
@@ -148,4 +127,38 @@ private fun Float.toIosSpeechRate(): Float {
         AVSpeechUtteranceMinimumSpeechRate,
         AVSpeechUtteranceMaximumSpeechRate
     )
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private class SpeechDelegate(
+    private val onStart: () -> Unit,
+    private val onFinish: () -> Unit,
+    private val onCancel: () -> Unit
+) : NSObject(), AVSpeechSynthesizerDelegateProtocol {
+
+    @ObjCSignatureOverride
+    override fun speechSynthesizer(
+        synthesizer: AVSpeechSynthesizer,
+        didStartSpeechUtterance: AVSpeechUtterance
+    ) {
+        onStart()
+    }
+
+    @ObjCSignatureOverride
+    override fun speechSynthesizer(
+        synthesizer: AVSpeechSynthesizer,
+        didFinishSpeechUtterance: AVSpeechUtterance
+    ) {
+        if (!synthesizer.speaking) {
+            onFinish()
+        }
+    }
+
+    @ObjCSignatureOverride
+    override fun speechSynthesizer(
+        synthesizer: AVSpeechSynthesizer,
+        didCancelSpeechUtterance: AVSpeechUtterance
+    ) {
+        onCancel()
+    }
 }
